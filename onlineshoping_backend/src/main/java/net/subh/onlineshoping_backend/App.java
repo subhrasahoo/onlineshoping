2d709d7d781dcff6d05680d69c1bd2f5c4dfee8c
SubhraSahoo <subhrasatyajit@gmail.com>
2019-03-24 15:02:39 +0530

    Subh's project setup completed successfully

---
+++ onlineshoping_backend/src/main/java/net/subh/onlineshoping_backend/App.java
@@ -0,0 +1,13 @@
+package net.subh.onlineshoping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
